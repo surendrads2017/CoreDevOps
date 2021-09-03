5c8ed60e3b9daf1a07c077275b3f0b598f04e775
Nakul Jain <nakul.jain@dotsquares.com>
2021-09-03 12:52:26 +0530

    Comitt first time

---
+++ CoreDevOps/Migrations/20210903061643_createDB.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreDevOps.Migrations
+{
+    public partial class createDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsComplete = table.Column<bool>(type: "bit", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 1L, "In Meeting need to discuss some points.", true, "Meeting with management" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 2L, "List of this this item buy.", false, "Go for shooping" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 3L, "Here is task to ask to do on call.", true, "Call to some one for do some task" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItem");
+        }
+    }
+}
---
+++ CoreDevOps/Models/TodoContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDevOps.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext()
+        {
+        }
+
+        public TodoContext(DbContextOptions<TodoContext> options): base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItem { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Server=88.99.153.130,1533;Database=CoreDevOpsDB;User ID=sa;Password=LWMu(9j%HcZ64;Integrated Security=false;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(
+                new TodoItem() { Id = 1, Name = "Meeting with management", IsComplete = true, Description = "In Meeting need to discuss some points." },
+                new TodoItem() { Id = 2, Name = "Go for shooping", IsComplete = false, Description = "List of this this item buy." },
+                new TodoItem() { Id = 3, Name = "Call to some one for do some task", IsComplete = true, Description = "Here is task to ask to do on call." });
+        }
+    }
+}
